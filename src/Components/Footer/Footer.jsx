77188fc2fcf9586ba8d02f691cd81ec93245d0ac
allan <allanotieno2001@gmail.com>
2025-12-27 14:43:21 +0300

    updates

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -17,26 +17,3 @@ const Footer = () => {
 }
 
 export default Footer
-
-// const onSubmit = async (event) => {
-  //   event.preventDefault();
-  //   setResult("Sending....");
-  //   const formData = new FormData(event.target);
-
-  //   formData.append("access_key", "fed806c4-1c51-4259-8d0b-eceeb1a51bae");
-
-  //   const response = await fetch("https://api.web3forms.com/submit", {
-  //     method: "POST",
-  //     body: formData
-  //   });
-
-  //   const data = await response.json();
-
-  //   if (data.success) {
-  //     setResult("Form Submitted Successfully");
-  //     event.target.reset();
-  //   } else {
-  //     console.log("Error", data);
-  //     setResult(data.message);
-  //   }
-  // };
