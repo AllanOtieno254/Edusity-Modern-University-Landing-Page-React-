17d27741c089f0dfa29e2c9933237dcfd62f460c
allan <allanotieno2001@gmail.com>
2025-12-27 12:47:07 +0300

    updated contact

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -11,6 +11,30 @@ import.meta.env.VITE_WEB3FORMS_ACCESS_KEY
 const Contact = () => {
   const [result, setResult] = useState("")
 
+  // const onSubmit = async (event) => {
+  //   event.preventDefault();
+  //   setResult("Sending....");
+  //   const formData = new FormData(event.target);
+
+  //   formData.append("access_key", "fed806c4-1c51-4259-8d0b-eceeb1a51bae");
+
+  //   const response = await fetch("https://api.web3forms.com/submit", {
+  //     method: "POST",
+  //     body: formData
+  //   });
+
+  //   const data = await response.json();
+
+  //   if (data.success) {
+  //     setResult("Form Submitted Successfully");
+  //     event.target.reset();
+  //   } else {
+  //     console.log("Error", data);
+  //     setResult(data.message);
+  //   }
+  // };
+
+
   const onSubmit = async (event) => {
     event.preventDefault()
     setResult("Sending...")
